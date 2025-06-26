6ef129dfbbf847ff9482377c24411d6b81cf422a
Joshuan777 <Joshuanmirandagonzalez@gmail.com>
2025-06-25 19:35:19 -0700

    erre

--- script.js
+++ script.js
@@ -2,9 +2,17 @@ let container = document.querySelector('.container');
 let nextPage = document.querySelector('.nextPage');
 let career = document.querySelector('.career');
 let home = document.querySelector('.home');
+let me = document.querySelector('.me')
+let aboutMe = document.querySelector('.aboutMe')
 
 
 nextPage.addEventListener('click', function(){
     container.style.display='none';
     home.style.display='block';
+});
+
+me.addEventListener('click', function(){
+    container.style.display='none';
+    home.style.display='none';
+    aboutMe.style.display='block';
 });
