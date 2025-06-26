66c44dfb3ad219e7ffc5b34f61ebdecc0bf1a7fe
Joshuan777 <Joshuanmirandagonzalez@gmail.com>
2025-06-26 06:40:52 -0700

    done

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let nextPage = document.querySelector('.nextPage');
 let career = document.querySelector('.career');
 let home = document.querySelector('.home');
 let me = document.querySelector('.me')
-let aboutMe = document.querySelector('.aboutMe')
+let aboutMe = document.querySelector('.aboutMe');
+let project = document.querySelector('.project');
+let work = document.querySelector('.work');
 
 
 nextPage.addEventListener('click', function(){
@@ -15,4 +17,11 @@ me.addEventListener('click', function(){
     container.style.display='none';
     home.style.display='none';
     aboutMe.style.display='block';
+});
+
+work.addEventListener('click',function(){
+    container.style.displau='none';
+    home.style.display='none';
+    aboutMe.style.display='none';
+    project.style.display='block';
 });
