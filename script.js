b567bc1d1c45dc7365209a9802320807fc7699ca
Joshuan777 <Joshuanmirandagonzalez@gmail.com>
2025-06-26 18:30:56 -0700

    erere

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ let me = document.querySelector('.me')
 let aboutMe = document.querySelector('.aboutMe');
 let project = document.querySelector('.project');
 let work = document.querySelector('.work');
+let contact = document.querySelector('.contact');
+let contactsP = document.querySelector('.contactsP');
 
 
 nextPage.addEventListener('click', function(){
@@ -25,3 +27,11 @@ work.addEventListener('click',function(){
     aboutMe.style.display='none';
     project.style.display='block';
 });
+
+contact.addEventListener('click', function(){
+    container.style.displau='none';
+    home.style.display='none';
+    aboutMe.style.display='none';
+    project.style.display='none';
+    contactsP.style.display='block';
+})
